8bdb21a83aa1175bd8933d9b029323ca000f251c
aaditya47 <50749476+aaditya47@users.noreply.github.com>
2019-12-20 09:38:46 +0530

    Added FCFS Algorithm for CPU Scheduling [OS]

---
+++ OS/CPU Scheduling/Driver.py
@@ -0,0 +1,13 @@
+import cpuscheduling as sc
+def main():
+    n=int(input("Enter the number of processes to be scheduled: "))
+    processes=[i for i in range(0,n)]
+    burst_time=[0]*n
+    for i in range(0,n):
+        print("Enter burst time of process %d: "%(i+1),end=' ')
+        burst_time[i]=int(input())
+    wt=sc.fcfs(processes,n,burst_time)
+    
+
+if(__name__=="__main__"):
+    main()
---
+++ OS/CPU Scheduling/cpuscheduling.py
@@ -0,0 +1,24 @@
+def fcfs_wt(processes, n, burst_time, waiting_time):
+    waiting_time[0]=0
+    for i in range(1,n):
+        waiting_time[i]=burst_time[i-1]+waiting_time[i-1]
+
+def fcfs_tat(processes, n, burst_time, waiting_time, turn_around_time):
+    for i in range(n):
+        turn_around_time[i]=burst_time[i]+waiting_time[i]
+
+def fcfs(processes, n, burst_time):
+    waiting_time=[0]*n
+    turn_around_time=[0]*n
+    total_wt=0
+    total_tat=0
+
+    fcfs_wt(processes, n, burst_time,waiting_time)
+
+    fcfs_tat(processes, n, burst_time, waiting_time, turn_around_time)
+
+    for i in range(0,n):
+        total_wt+=waiting_time[i]
+    
+    print("Average Waiting Time = ",str(total_wt/n))
+    return (total_wt/n)
